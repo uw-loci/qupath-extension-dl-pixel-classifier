a15e2f17557dda763ce9ef855b4a3ff9fca96d62
MichaelSNelson <imagescientistwebsite@gmail.com>
2026-06-09 17:12:35 -0500

    Release 0.8.3

--- src/main/java/qupath/ext/dlclassifier/service/ApposeService.java
+++ src/main/java/qupath/ext/dlclassifier/service/ApposeService.java
@@ -923,7 +923,7 @@ public class ApposeService {
      * </ol>
      */
     /** Extension version. Used for pip URL construction and script generation. */
-    public static final String DL_SERVER_VERSION = "0.8.3-dev";
+    public static final String DL_SERVER_VERSION = "0.8.3";
 
     private static final boolean IS_DEV_BUILD = DL_SERVER_VERSION.contains("-dev");
     private static final String DL_SERVER_PIP_URL;
