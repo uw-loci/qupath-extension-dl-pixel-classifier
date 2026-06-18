4b8541dd3a7a627ba4de5fbfc56875c15768418e
MichaelSNelson <imagescientistwebsite@gmail.com>
2026-06-18 16:27:11 -0500

    Start 0.8.5-dev cycle

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 qupathExtension {
     name = "qupath-extension-dl-pixel-classifier"
     group = "io.github.uw-loci"
-    version = "0.8.4"
+    version = "0.8.5-dev"
     description = "Deep learning pixel classifier for QuPath supporting multi-channel images."
     automaticModule = "io.github.uw-loci.extension.dlclassifier"
 }
